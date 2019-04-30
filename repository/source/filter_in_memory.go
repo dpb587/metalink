885d51c4467d17e6893d079723c2534533fb84cd
Danny Berger <dpb587@gmail.com>
2019-04-29 20:17:33 -0700

    Include meta4 path in filtering errors

--- repository/source/filter_in_memory.go
+++ repository/source/filter_in_memory.go
@@ -1,9 +1,9 @@
 package source
 
 import (
-	"github.com/pkg/errors"
 	"github.com/dpb587/metalink/repository"
 	"github.com/dpb587/metalink/repository/filter"
+	"github.com/pkg/errors"
 )
 
 func FilterInMemory(files []repository.RepositoryMetalink, filter filter.Filter) ([]repository.RepositoryMetalink, error) {
@@ -12,7 +12,7 @@ func FilterInMemory(files []repository.RepositoryMetalink, filter filter.Filter)
 	for _, meta4 := range files {
 		matched, err := filter.IsTrue(meta4)
 		if err != nil {
-			return nil, errors.Wrap(err, "Matching metalink")
+			return nil, errors.Wrapf(err, "Matching metalink %s", meta4.Reference.Path)
 		} else if !matched {
 			continue
 		}
