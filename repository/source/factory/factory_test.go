df7aa32b887e0b0637c8b459dd7ee628762459c0
Joseph Palermo <jpalermo@vmware.com>
2023-05-15 16:40:25 -0700

    Change format of git+ssh URIs
    Previously, you could pass URIs like:
    git+ssh://user@host:some-org/some-repo
    
    This was parsed by `net/url` prior to Golang 1.8, even though it is not
    actually a valid URL. Post Golang 1.8, you will receive an error when
    trying to parse that string as a URL.
    
    Rather than write our own parser, we have changed the format accepted.
    You must now pass something like the following (note that there is a
    slash now instead of a colon after host):
    git+ssh://user@host/some-org/some-repo
    
    This is a parseable URL. The `git.Factory` class reformats this URL so
    that it can be passed as the repository arguments to `git clone` within
    `git.Source`. This "new" format is actually what was originally
    documented in _package.go as an example for using git+ssh scheme.
    
    [#185022387] Add AssumeRole support to metalink
    
    Signed-off-by: Brian Upton <bupton@vmware.com>

---
+++ repository/source/factory/factory_test.go
@@ -0,0 +1,59 @@
+package factory_test
+
+import (
+	boshlog "github.com/cloudfoundry/bosh-utils/logger"
+	boshsys "github.com/cloudfoundry/bosh-utils/system"
+	"github.com/dpb587/metalink/repository/source/factory"
+	source_fs "github.com/dpb587/metalink/repository/source/fs"
+	source_git "github.com/dpb587/metalink/repository/source/git"
+	source_http "github.com/dpb587/metalink/repository/source/http"
+	source_s3 "github.com/dpb587/metalink/repository/source/s3"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Factory", func() {
+	var subject *factory.Factory
+
+	Describe("Create", func() {
+		BeforeEach(func() {
+			logger := boshlog.NewLogger(boshlog.LevelError)
+			fs := boshsys.NewOsFileSystem(logger)
+			cmdRunner := boshsys.NewExecCmdRunner(logger)
+
+			subject = factory.NewFactory()
+			subject.Add(source_fs.NewFactory(fs))
+			subject.Add(source_http.NewFactory())
+			subject.Add(source_git.NewFactory(fs, cmdRunner))
+			subject.Add(source_s3.NewFactory())
+		})
+
+		It("supports file URLs", func() {
+			source, err := subject.Create("file:///some/file/path", map[string]interface{}{})
+			Expect(err).ToNot(HaveOccurred())
+			_, isCorrectType := source.(*source_fs.Source)
+			Expect(isCorrectType).To(BeTrue())
+		})
+
+		It("supports HTTP URLs", func() {
+			source, err := subject.Create("https://some.url", map[string]interface{}{})
+			Expect(err).ToNot(HaveOccurred())
+			_, isCorrectType := source.(*source_http.Source)
+			Expect(isCorrectType).To(BeTrue())
+		})
+
+		It("supports S3 URLs", func() {
+			source, err := subject.Create("s3://s3.amazonaws.com/some-bucket/some-file", map[string]interface{}{})
+			Expect(err).ToNot(HaveOccurred())
+			_, isCorrectType := source.(*source_s3.Source)
+			Expect(isCorrectType).To(BeTrue())
+		})
+
+		It("supports git SSH URLs", func() {
+			source, err := subject.Create("git+ssh://github.com/some-org/some-repo", map[string]interface{}{})
+			Expect(err).ToNot(HaveOccurred())
+			_, isCorrectType := source.(*source_git.Source)
+			Expect(isCorrectType).To(BeTrue())
+		})
+	})
+})
